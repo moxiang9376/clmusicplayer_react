6e937ee5e319177de2307f73a9162f4cf0a01607
mo <908935695@qq.com>
2020-05-28 15:04:47 +0800

    20205.5.28 --1

--- src/router.js
+++ src/router.js
@@ -1,19 +1,15 @@
-import React from "react";
+//引入react jsx写法的必须
+import React from 'react';
+//引入需要用到的页面组件 
+import homePage from './views/homePage/homePage';
+//引入一些模块
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./views/home/home";
-import MusicList from "./views/musicList/musicList";
-import Tabbar from "./component/tabbar/tabbar";
-import HeadNav from "./component/headNav/headNav";
-import AudioPlayer from "./component/audioPlayer/audioPlayer"
 
-const BasicRoute = () => (
-  <Router>
-    <HeadNav></HeadNav>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/musicList" component={MusicList} />
-    <AudioPlayer></AudioPlayer>
-    <Tabbar></Tabbar>
-  </Router>
-);
+function router() {
+    return (
+        <Router>
+            <Route path="/" component={homePage} />
+        </Router>);
+}
 
-export default BasicRoute;
+export default router;
---
+++ src/views/homePage/homePage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import "./homePage.scss"
+
+export default class Home extends Component {
+  render() {
+    return (<h1>
+      欢迎，这里是Home
+    </h1>)
+  }
+}
